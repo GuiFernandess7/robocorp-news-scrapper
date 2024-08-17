66e466fcc17e4db402db2bf6efc0e138eb7c5e9d
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-08-17 17:38:11 -0300

    url updated

--- scraper.py
+++ scraper.py
@@ -15,7 +15,7 @@ class NewsScraper:
 
     def set_chrome_options(self):
         options = Options()
-        #options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-extensions')
         options.add_argument('--disable-gpu')
--- tasks.py
+++ tasks.py
@@ -4,7 +4,7 @@ import time
 
 @task
 def minimal_task():
-    url = "https://apnews.com/"
+    url = "https://www.aljazeera.com/"
     scraper = NewsScraper()
     scraper.set_webdriver()
     scraper.open_url(url)
