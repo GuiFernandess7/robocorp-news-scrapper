67043981c8a8e42f0547217543f3068c019c3b2e
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-08-17 16:08:17 -0300

    config: headless mode added

--- scraper.py
+++ scraper.py
@@ -2,9 +2,12 @@
 
 from RPA.Browser.Selenium import Selenium
 from robocorp import workitems
+from selenium.webdriver import FirefoxOptions
 
 class NewsScraper:
     def __init__(self):
+        opts = FirefoxOptions()
+        opts.add_argument("--headless")
         self.browser = Selenium()
 
     def open_browser(self, url):
@@ -18,4 +21,4 @@ class NewsScraper:
         return item
 
     def set_work_item(self, key, value):
-        self.set_variable(key, value)
+        self.browser.set_variable(key, value)
