066712b5186606951ce63d196ea274fde595af9e
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-08-17 17:35:18 -0300

    time sleep added

--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 from robocorp.tasks import task
 from scraper import NewsScraper
+import time
 
 @task
 def minimal_task():
@@ -7,4 +8,5 @@ def minimal_task():
     scraper = NewsScraper()
     scraper.set_webdriver()
     scraper.open_url(url)
+    time.sleep(3.0)
     scraper.driver_quit()
