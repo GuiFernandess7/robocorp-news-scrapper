acc80200030e1b08f934c3afd43db21a0b467bf1
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-08-18 16:57:23 -0300

    feat: Get results

--- tasks.py
+++ tasks.py
@@ -1,11 +1,33 @@
 from robocorp.tasks import task
+from RPA.Robocorp.WorkItems import WorkItems
 from scraper import NewsScraper
-import time
+
+import os
+import logging
 
 @task
-def minimal_task():
-    url = "https://apnews.com/"
+def run_news_task():
+    url = os.getenv('URL', 'https://apnews.com/')
+    search_phrase = os.getenv('search_phrase', "Artificial Intelligence")
+
     scraper = NewsScraper()
     scraper.set_webdriver()
     scraper.open_url(url)
+
+    scraper.search(search_phrase)
+    results = scraper.get_results()
+    scraper.store_results(results, search_phrase)
+
     scraper.driver_quit()
+
+if __name__=="__main__":
+    run_news_task()
+
+    """
+    library = WorkItems()
+        library.get_input_work_item()
+
+        variables = library.get_work_item_variables()
+        for variable, value in variables.items():
+            print("%s = %s", variable, value)
+    """
