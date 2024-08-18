563bebd5db021a3f18bd67064e171ab4d9b8af4b
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-08-18 18:49:56 -0300

    config: Chrome options updated

--- scraper.py
+++ scraper.py
@@ -42,11 +42,10 @@ class NewsScraper:
         options = Options()
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
-        options.add_argument('--disable-extensions')
-        options.add_argument('--disable-gpu')
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-gpu")
         options.add_argument('--disable-web-security')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--start-maximized')
+        options.add_argument("--start-maximized")
         options.add_argument('--remote-debugging-port=9222')
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
         return options
@@ -67,10 +66,6 @@ class NewsScraper:
         if not self.driver:
             self.logger.error("Driver not initialized.")
             raise RuntimeError("Driver not initialized.")
-
-    def set_page_size(self, width: int, height: int):
-        self.__check_driver()
-        self.driver.set_window_size(width, height)
 
     def open_url(self, url: str, screenshot: str = None):
         self.__check_driver()
